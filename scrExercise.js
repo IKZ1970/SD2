94381ab9e935506208d22a4a93985276f39726ee
IKZ1970 <zadruzynski@gmx>
2022-02-16 16:39:50 +0000

    no message

---
+++ scrExercise.js
@@ -0,0 +1,9 @@
+function toRed(){
+    document.getElementById("theText").style.color = "red";
+}
+function toBlue(){
+    document.getElementById("theText").style.color = "blue";
+}
+function toBlack(){
+    document.getElementById("theText").style.color = "black";
+}
---
+++ script2.js
@@ -0,0 +1,6 @@
+function msg() {
+    document.write("You clicked the button!");
+}
+function msg2() {
+    document.write("Not that button!")
+}
